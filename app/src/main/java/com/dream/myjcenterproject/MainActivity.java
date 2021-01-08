0d915784cd6fe82d90c7d93a8b5cb485d03e7e68
zhouying <1770385837@qq.com>
2021-01-08 16:04:42 +0800

    location相关业务代码提交

--- app/src/main/java/com/dream/myjcenterproject/MainActivity.java
+++ app/src/main/java/com/dream/myjcenterproject/MainActivity.java
@@ -4,11 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.location.sweetying.SmartLocation;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SmartLocation.location();
     }
 }
