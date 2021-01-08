a8123959f2848a53e0aa9fbb94f553ad188fb9de
zhouying <1770385837@qq.com>
2021-01-08 19:39:51 +0800

    location库版本升级

--- SmartLocation/src/main/java/com/location/sweetying/SmartLocation.java
+++ SmartLocation/src/main/java/com/location/sweetying/SmartLocation.java
@@ -12,6 +12,7 @@ public class SmartLocation {
 
     public static final String TAG = "SmartLocation";
 
+    /**添加一行注释*/
     public static void location(){
         Log.d(TAG, "location: 开启智能定位");
     }
